b6638075e95879ede31b75b941d31ba7fc48d633
JFMajer <jakubmajer@live.com>
2023-05-06 13:53:02 +0200

    working configuration

--- src/main/java/com/polarbookshop/edgeservice/EdgeServiceApplication.java
+++ src/main/java/com/polarbookshop/edgeservice/EdgeServiceApplication.java
@@ -8,6 +8,7 @@ public class EdgeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdgeServiceApplication.class, args);
+		System.out.println("CATALOG_SERVICE_URI is set to: " + System.getenv("CATALOG_SERVICE_URI"));
 	}
 
 }
