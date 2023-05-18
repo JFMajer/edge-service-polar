999277b344caf1763645dbca863c450eb56686f3
JFMajer <jakubmajer@live.com>
2023-05-18 20:53:21 +0200

    change in redis settings

--- src/main/java/com/polarbookshop/edgeservice/EdgeServiceApplication.java
+++ src/main/java/com/polarbookshop/edgeservice/EdgeServiceApplication.java
@@ -9,6 +9,7 @@ public class EdgeServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EdgeServiceApplication.class, args);
 		System.out.println("CATALOG_SERVICE_URI is set to: " + System.getenv("CATALOG_SERVICE_URI"));
+		System.out.println("REDIS_HOST is set to: " + System.getenv("REDIS_URI"));
 	}
 
 }
